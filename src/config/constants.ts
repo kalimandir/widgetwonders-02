e2a862d7c9d44a674e9ab3a2f4e53ee11802b026
Andreas Oee <andy.oeee@gmail.com>
2025-03-25 20:02:47 +0700

    fix: replace receiver with alecity

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,8 +4,9 @@ const IS_DEV = process.env.NODE_ENV === "development";
 export const PORT = 8080;
 
 export const CONFIG = {
-  RECIPIENT_ENS: "hardforksailor5.yodl.eth",
-  RECIPIENT_ADDRESS: "0xB34021082BDcD7D774E87e2ABD731f3D37fa75D3" as Hex,
+  // RECIPIENT_ENS: "hardforksailor5.yodl.eth",
+  RECIPIENT_ENS: "alecity.eth",
+  RECIPIENT_ADDRESS: "0xDC3Cac69C81161ab8Fea6AB84fE90a7ECa43912A" as Hex, // must match RECIPIENT_ENS
   YAPP_URL: IS_DEV ? `http://localhost:${PORT}` : "https://donation-yapp.vercel.app",
   YAPP_ENS: "donation-yapp.yodl.eth",
   HOST: IS_DEV ? "http://localhost:3000" : "https://dapp-git-sb-add-user-context-request-yodl.vercel.app/go",

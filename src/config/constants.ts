7de83850d7bea3f548f18cf860792fec24130727
Andreas Oee <andy.oeee@gmail.com>
2025-03-26 14:39:36 +0700

    fix: replace host url

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -9,6 +9,6 @@ export const CONFIG = {
   RECIPIENT_ADDRESS: "0xDC3Cac69C81161ab8Fea6AB84fE90a7ECa43912A" as Hex, // must match RECIPIENT_ENS
   YAPP_URL: IS_DEV ? `http://localhost:${PORT}` : "https://donation-yapp.vercel.app",
   YAPP_ENS: "donation-yapp.yodl.eth",
-  HOST: IS_DEV ? "http://localhost:3000" : "https://dapp-git-sb-add-user-context-request-yodl.vercel.app/go",
+  HOST: IS_DEV ? "http://localhost:3000" : "https://yodl.me",
   REDIRECT_URL: IS_DEV ? `http://localhost:${PORT}` : "https://donation-yapp.vercel.app",
 };
